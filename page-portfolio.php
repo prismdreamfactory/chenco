29d58900377870f382079b1be40e16a71bdeb387
Eric K Wu <ekuan.wu@gmail.com>
2019-11-17 00:45:18 -0800

    Create portfolio page template

---
+++ page-portfolio.php
@@ -0,0 +1,55 @@
+<?php
+
+/* Template Name: Portfolio Page */
+
+if (!defined('ABSPATH')) {
+  exit; // Exit if accessed directly.
+}
+
+get_header(); ?>
+
+<div id="primary" <?php generate_do_element_classes('content'); ?>>
+  <main id="main" <?php generate_do_element_classes('main'); ?>>
+    <?php
+    /**
+     * generate_before_main_content hook.
+     *
+     * @since 0.1
+     */
+    do_action('generate_before_main_content'); ?>
+
+    <div class="grid-container">
+
+      <h1>Portfolio</h1>
+
+      <p>Our portfolio spans eighteen states</p>
+
+    </div>
+
+    <?php while (have_posts()) : the_post();
+
+      get_template_part('partials/map');
+
+    endwhile;
+
+    /**
+     * generate_after_main_content hook.
+     *
+     * @since 0.1
+     */
+    do_action('generate_after_main_content');
+    ?>
+  </main><!-- #main -->
+</div><!-- #primary -->
+
+<?php
+/**
+ * generate_after_primary_content_area hook.
+ *
+ * @since 2.0
+ */
+do_action('generate_after_primary_content_area');
+
+generate_construct_sidebars();
+
+get_footer();
