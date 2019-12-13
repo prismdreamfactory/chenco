06a69de4064d728aaacdfe99dc93b693cd2703fb
Eric K Wu <ekuan.wu@gmail.com>
2019-12-13 13:37:50 -0800

    More mobile nav and partners styling

--- js/script.js
+++ js/script.js
@@ -32,7 +32,19 @@ function debounced(delay, fn) {
         slidesToShow: 4,
         responsive: [
           {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3
+            }
+          },
+          {
             breakpoint: 768,
+            settings: {
+              slidesToShow: 2
+            }
+          },
+          {
+            breakpoint: 480,
             settings: {
               slidesToShow: 1
             }
