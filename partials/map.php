4944cc4168adec3efdfed4e4348cb3857641bf20
Winston Wu <pfunto.wu@gmail.com>
2019-12-17 18:09:32 -0800

    merged develop into contact

--- partials/map.php
+++ partials/map.php
@@ -9,13 +9,12 @@
 <section class="map">
 
   <div class="map__tabs">
-    <div class="map__tab font--alt">Global</div>
-    <div class="map__tab font--alt mod--active">U.S. Properties</div>
-    <div class="map__tab font--alt">Asia Properties</div>
+    <div class="map__tab font--alt" data-center="global">Global</div>
+    <div class="map__tab font--alt mod--active" data-center="usa">U.S. Properties</div>
+    <div class="map__tab font--alt" data-center="asia">Asia Properties</div>
   </div>
 
   <div class="map-wrapper">
-
     <div id="map">
 
       <?php
@@ -25,27 +24,46 @@
           'posts_per_page' => -1,
         )
       );
-      while ($loop->have_posts()) : $loop->the_post();
-        // $marker = get_sub_field('marker'); 
-
-        ?>
+      while ($loop->have_posts()) : $loop->the_post(); ?>
 
       <?php if (have_rows('location')) : the_row();
             $lat = get_sub_field('latitude');
             $lng = get_sub_field('longitude');
             $type = get_field('asset_type');
+            $units = get_field('units');
+            $sqft = get_field('sqft');
+            $acres = get_field('acres');
+
+            $current = get_field('current_property', false, false);
+
+            if ($type['value'] == 'O') {
+              $officeSqft = get_field('sqft');
+              $indSqft = '';
+            } elseif ($type['value'] == 'Ind') {
+              $officeSqft = '';
+              $indSqft = get_field('sqft');
+            } else {
+              $officeSqft = '';
+              $indSqft = '';
+            }
+
+            $stats = $officeSqft . "," . get_field('units') . "," . get_field('acres') . "," . $indSqft;
             ?>
 
-      <div class="marker" style="display: none;" data-lat="<?= $lat; ?>" data-lng="<?= $lng; ?>">
+      <div class="marker" style="display: none;" data-lat="<?= $lat; ?>" data-lng="<?= $lng; ?>"
+        data-type="<?= $type['label'] ?>" data-current="<?= $current; ?>" data-stats="<?= $stats ?>">
         <div class="map__info">
           <h3 class="map__info__heading"><?php echo $type['label']; ?>
           </h3>
           <p class="map__info__item">-
-            <?php if (get_field('sqft')) : ?>
-            <span><?php the_field('sqft'); ?> sq ft</span>
+            <?php if ($units) : ?>
+            <span class="units"><?php echo number_format($units); ?> Units</span>
             <?php endif; ?>
-            <?php if (get_field('units')) : ?>
-            <span><?php the_field('units') ?> units</span>
+            <?php if ($sqft) : ?>
+            <span class="sqft"><?php echo number_format($sqft); ?> Sq. Ft.</span>
+            <?php endif; ?>
+            <?php if ($acres) : ?>
+            <span class="acres"><?php echo number_format($acres); ?> Acres</span>
             <?php endif; ?>
           </p>
 
@@ -61,18 +79,30 @@
 
     <div class="map__legend">
       <div class="map__legend--main">
-        <h3>California</h3>
-        <p class="map__legend-row"><span>924,528,127 sq ft.</span><span>Office Properties</span></p>
-        <p class="map__legend-row"><span>145 units</span><span>Multi-Family Properties</span></p>
+        <h3 class="map__legend-region">United States</h3>
+        <p class="map__legend-row mod--office"><span><i>5,900,000</i> Sq. Ft.</span><span>Commercial Properties</span>
+        </p>
+        <p class="map__legend-row mod--multifamily"><span><i>24,000</i> Units</span><span>Multifamily Properties</span>
+        </p>
+        <p class="map__legend-row mod--land"><span><i>4,000</i> Acres</span><span>Land Acres</span></p>
+        <p class="map__legend-row mod--industrial"><span><i>3,540,000</i> Sq. Ft.</span><span>Industrial
+            Properties</span>
+        </p>
       </div>
       <div class="map__legend--bottom">
-        <p class="map__legend--bottom-text">Legend: <span>Office</span> <span>Multi-Family</span></p>
+        <label class="legend__label">Legend:</label>
+        <ul class="legend__list">
+          <li class="legend__list-item mod--office">Office (SF)</li>
+          <li class="legend__list-item mod--multifamily">Multifamily (Units)</li>
+          <li class="legend__list-item mod--land">Land (Acres)</li>
+          <li class="legend__list-item mod--industrial">Industrial (SF)</li>
+        </ul>
       </div>
     </div>
 
     <div class="map__switch">
-      <div class="map__switch-item mod--active"><i></i>Current</div>
-      <div class="map__switch-item"><i></i>Historical</div>
+      <div class="map__switch-item mod--active" data-current="true"><i></i>Current</div>
+      <div class="map__switch-item" data-current="false"><i></i>Historical</div>
     </div>
 
   </div>
